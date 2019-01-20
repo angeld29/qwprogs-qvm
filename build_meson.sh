e2e84d487fba06e0d186ec5ae31d0f39d87e0df1
angeld <roman.andriyanov@corp.mail.ru>
2019-01-20 12:37:22 +0300

    crossbuild

---
+++ build_meson.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+rm -rf _build
+rm -rf _buildout
+mkdir _build
+mkdir _buildout
+for name in linux-amd64 linux-armhf linux-i686 windows-x64 windows-x86; do
+    mkdir _build/$name
+    mkdir _buildout/$name
+    meson _build/$name --cross-file tools/cross-compilation/$name.txt
+    ninja -C _build/$name
+    cp _build/$name/qwprogs.* _buildout/$name/
+done;
+
+
