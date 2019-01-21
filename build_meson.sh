4b3d6114b63c3979e3aeef07dda3f02538b76156
angeld <angeld@kitezhgrad5.bk.clustertech.ru>
2019-01-21 07:45:46 +0300

    api12

--- build_meson.sh
+++ build_meson.sh
@@ -6,7 +6,7 @@ mkdir _buildout
 for name in linux-amd64 linux-armhf linux-i686 windows-x64 windows-x86; do
     mkdir _build/$name
     mkdir _buildout/$name
-    meson _build/$name --cross-file tools/cross-compilation/$name.txt
+    meson --buildtype=release _build/$name --cross-file tools/cross-compilation/$name.txt
     ninja -C _build/$name
     cp _build/$name/qwprogs.* _buildout/$name/
 done;
